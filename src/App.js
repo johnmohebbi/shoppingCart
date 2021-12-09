713b19f9fcba55e17b09519cb4a4c8b0c50f44cb
john Mohebbi <mohebbijohn@gmail.com>
2021-12-09 12:43:56 +0330

    cteate productsContext and get product from api and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+//context 
+import ProductsContextProvider from "./context/ProductsContextProvider";
 function App() {
   
   return (
     <div >
-   
+      <ProductsContextProvider>
+      </ProductsContextProvider>
     </div>
   );
 }
---
+++ src/context/ProductsContextProvider.js
@@ -0,0 +1,23 @@
+import React,{ createContext, useEffect, useState }from 'react';
+
+//APIs
+import { getProducts } from '../services/api';
+export const ProductsContext = createContext ();
+const ProductsContextProvider = ({children}) => {
+    const [products,setProducts] = useState([])
+    useEffect(()=>{
+        const fetchAPI = async () => {
+            setProducts(await getProducts())
+        }
+        fetchAPI()
+    
+    },[])
+
+    return (
+        <ProductsContext.Provider value ={products}>
+            {children}
+        </ProductsContext.Provider>
+    );
+};
+
+export default ProductsContextProvider;
