09ee0e5c5b12aafec06da308e329948892729c2b
john Mohebbi <mohebbijohn@gmail.com>
2021-12-14 13:01:57 +0330

    added style to details.js

--- src/components/shared/Details.js
+++ src/components/shared/Details.js
@@ -4,25 +4,28 @@ import {Link, useParams} from 'react-router-dom';
 
 //context
 import { ProductsContext } from '../../context/ProductsContextProvider';
-import { shorten } from '../../helper/functions';
+// import { shorten } from '../../helper/functions';
+//css
+import styles from './styles/details.module.css'
 const Details = () => {
     const {id} = useParams()
     const products = useContext(ProductsContext);
     const dataProduct = products[id - 1]
     return (
-        <div>
-            
-            <div>
-                <img src={dataProduct.image} alt="imageDetails" width="200px" />
-                <div>
-                    <h3><span>title:</span>{shorten(dataProduct.title)}</h3>
-                    <p><span>category:</span> {dataProduct.category}</p>
-                    <p><span>category:</span> {dataProduct.description}</p>
-                    <p>{dataProduct.price}</p>
-                </div>
-            <Link to="/products">go to products</Link>
+        
+        <div className={styles.detailsContainer}>
+            <img src={dataProduct.image} alt="imageDetails" />
+            <div className={styles.detailsBody}>
+                <h3><span>title:</span>{dataProduct.title}</h3>
+                <p className={styles.detailsDescription}><span>description:</span> {dataProduct.description}</p>
+                <p className={styles.detailsCategory}><span>category:</span> {dataProduct.category}</p>
+                <section>
+                    <p>{dataProduct.price} $</p>
+                    <Link to="/products">go to products</Link>
+                </section>
             </div>
         </div>
+        
     );
 };
 
